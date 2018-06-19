fc1f82fdfb7e4ab3bb6af28a792b402c3b69c1db
Fernando Migliorini Tenório <fernando.tenorio@tecnospeed.com.br>
2018-06-19 14:07:06 -0300

    beta layout version

---
+++ www/js/index.js
@@ -0,0 +1,32 @@
+var colors = [
+  {
+    title: 'Amarelo',
+    backgroundColor: 'yellow-300',
+    buttonColor: 'yellow-800',
+    points: 5
+  },
+  {
+    title: 'Verde',
+    backgroundColor: 'green-300',
+    buttonColor: 'green-800',
+    points: 10
+  },
+  {
+    title: 'Azul',
+    backgroundColor: 'blue-300',
+    buttonColor: 'blue-800',
+    points: 35
+  },
+  {
+    title: 'Vermelho',
+    backgroundColor: 'red-300',
+    buttonColor: 'red-800',
+    points: 50
+  },
+  {
+    title: 'Preto',
+    backgroundColor: 'grey-900',
+    buttonColor: 'grey-300',
+    points: 100
+  }
+]
