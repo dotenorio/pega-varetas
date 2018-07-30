f9b2a14a4d8db09f816e65a34fe7b80af328a08b
Fernando Milgiorini Tenório <dotenorio@gmail.com>
2018-07-30 14:02:03 -0300

    fix: no scroll FTW

--- www/js/index.js
+++ www/js/index.js
@@ -5,35 +5,40 @@ var colors = [
     title: 'Amarelo',
     backgroundColor: 'yellow-300',
     buttonColor: 'yellow-800',
-    points: 5
+    points: 5,
+    qty: 12
   },
   {
     id: 'green',
     title: 'Verde',
     backgroundColor: 'green-300',
     buttonColor: 'green-800',
-    points: 10
+    points: 10,
+    qty: 6
   },
   {
     id: 'blue',
     title: 'Azul',
     backgroundColor: 'blue-300',
     buttonColor: 'blue-800',
-    points: 35
+    points: 35,
+    qty: 6
   },
   {
     id: 'red',
     title: 'Vermelho',
     backgroundColor: 'red-300',
     buttonColor: 'red-800',
-    points: 50
+    points: 50,
+    qty: 12
   },
   {
     id: 'black',
     title: 'Preto',
     backgroundColor: 'black',
     buttonColor: 'grey-300',
-    points: 100
+    points: 100,
+    qty: 1
   }
 ]
 var players = []
